31d1697af00ae0ce829d0cd0721b687cdde4269e
Abid-Noyon <abidnoyon49@gmail.com>
2023-07-22 18:40:57 +0600

    update

---
+++ function/script.js
@@ -0,0 +1,38 @@
+let firstName = "Abid"
+let lastName = "Noyon"
+
+let fullName = firstName + " " + lastName
+
+console.log(fullName) 
+
+let name = "Linda"
+let greeting = "Hi there"
+
+let fGreeting = greeting + "," + " " + name
+
+console.log(fGreeting)
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Points() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+
+console.log(myPoints)
+
+console.log("2" + 2) // "22"
+console.log(11 + 7) // 18
+console.log(6 + "5") // "65"
+console.log("My points: " + 5 + 9) // "My points: 59"
+console.log(2 + 2) // 4
+console.log("11" + "14") // "1114"
